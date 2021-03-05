01256511aff98150c54a5aa39f56e70e478cd1f0
SuperSaiyanAsian <25292225+SuperSaiyanAsian@users.noreply.github.com>
2021-03-05 01:29:34 -0800

    Implemented method to pass third test
    To output whether a given year is a leap year, I included an additional if statement that checks if the given year is divisible by 400. If the condition is met, the code outputs that the given year is a leap year.

--- HW7/LeapYear.py
+++ HW7/LeapYear.py
@@ -2,5 +2,8 @@ def leap(year):
     if(year % 4 == 0 and year % 100 != 0):
         return("Year " + str(year) + " is a leap year.")
     
+    elif(year % 400 == 0):
+        return("Year " + str(year) + " is a leap year.")
+
     else:
         return("Year " + str(year) + " is not a leap year.")
