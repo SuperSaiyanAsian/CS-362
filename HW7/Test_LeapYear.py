e10524d3f5d5a34e68f60c2893bd4393c6847498
SuperSaiyanAsian <25292225+SuperSaiyanAsian@users.noreply.github.com>
2021-03-05 01:11:44 -0800

    Wrote second test
    This test checks if the code outputs that the given year is a leap year only if it is divisible by four and not also divisible by 100.

--- HW7/Test_LeapYear.py
+++ HW7/Test_LeapYear.py
@@ -7,5 +7,8 @@ class TestLeapYear(unittest.TestCase):
         self.assertEqual(LeapYear.leap(420), "Year 420 is a leap year.")
         self.assertEqual(LeapYear.leap(421), "Year 421 is not a leap year.")
 
+    def test_2(self):
+        self.assertEqual(LeapYear.leap(300), "Year 300 is not a leap year.")
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
