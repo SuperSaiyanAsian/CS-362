d1fbe62f008e6d41cc17af3712f05fcce2157203
SuperSaiyanAsian <25292225+SuperSaiyanAsian@users.noreply.github.com>
2021-03-05 00:41:07 -0800

    Implemented method to pass third test
    To replace multiples of three with "Fizz", multiples of five with "Buzz", and multiples of both three and five with "FizzBuzz", I used if statements which check if the number of the current iteration is divisible by three but not five, five but not three, and fifteen, respectively. If the condition is met, the appropriate replacement is concatenated to the string.

--- HW7/FizzBuzz.py
+++ HW7/FizzBuzz.py
@@ -2,13 +2,19 @@ def fizzbuzz():
     OneHundred = ""
 
     for i in range(1, 101):
-        if(i % 3 == 0):
+        if(i % 3 == 0 and i % 5 != 0):
             OneHundred = OneHundred + "Fizz "
+        
+        elif(i % 3 != 0 and i % 5 == 0):
+            if(i == 100):
+                OneHundred = OneHundred + "Buzz"
+            else:
+                OneHundred = OneHundred + "Buzz "
+            
+        elif(i % 15 == 0):
+            OneHundred = OneHundred + "FizzBuzz "
 
         else:
-            if(i == 100):
-                OneHundred = OneHundred + str(i)
-            else:
-                OneHundred = OneHundred + str(i) + " "
+            OneHundred = OneHundred + str(i) + " "
 
     return OneHundred
