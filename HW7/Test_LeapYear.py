a01364f1ce7d0abf7d8effe6d85cb9e25e25a830
SuperSaiyanAsian <25292225+SuperSaiyanAsian@users.noreply.github.com>
2021-03-05 00:57:52 -0800

    Wrote first test
    This test checks if the code outputs that the given year is a leap year only if it is divisible by 4.

---
+++ HW7/Test_LeapYear.py
@@ -0,0 +1,11 @@
+import unittest
+import LeapYear
+
+class TestLeapYear(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(LeapYear.leap(420), "Year 420 is a leap year.")
+        self.assertEqual(LeapYear.leap(421), "Year 421 is not a leap year.")
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
